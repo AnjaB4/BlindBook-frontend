fef62fe70745ea60a11e04a1243c97f3d17b325a
AnjaB <221894507+AnjaB4@users.noreply.github.com>
2026-08-27 18:24:59 +0200

    Update main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import { removeToken } from "../utils/token"
 
+
 function MainLayout({ children }) {
     const navigate = useNavigate()
 
